ad6a606b138f0ad5b0cd1cffd2e9d00f519d1639
Seth Hostetter <51931648+shostetter@users.noreply.github.com>
2021-05-28 06:52:18 -0400

    wip: left off setting up attrs
    todo: run method

--- producers/models/weather.py
+++ producers/models/weather.py
@@ -30,16 +30,13 @@ class Weather(Producer):
     summer_months = set((6, 7, 8))
 
     def __init__(self, month):
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
+        topic_name = f"cta.weather"
         super().__init__(
-            "weather", # TODO: Come up with a better topic name
+            topic_name
             key_schema=Weather.key_schema,
             value_schema=Weather.value_schema,
+            num_partitions=1, 
+            num_replicas=1
         )
 
         self.status = Weather.status.sunny
@@ -53,9 +50,6 @@ class Weather(Producer):
             with open(f"{Path(__file__).parents[0]}/schemas/weather_key.json") as f:
                 Weather.key_schema = json.load(f)
 
-        #
-        # TODO: Define this value schema in `schemas/weather_value.json
-        #
         if Weather.value_schema is None:
             with open(f"{Path(__file__).parents[0]}/schemas/weather_value.json") as f:
                 Weather.value_schema = json.load(f)
